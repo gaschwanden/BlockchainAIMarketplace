e43cb298bd08854f2c432c23561a1734fd0541e8
Kevin Ma <1043258912@qq.com>
2018-04-29 16:36:55 +1000

    Try to connect web3 with react

--- src/js/index.js
+++ src/js/index.js
@@ -1,54 +1,69 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Web3 from 'web3'
 import './../css/index.css'
 import 'antd/dist/antd.css';
 // import { AppContainer } from 'react-hot-loader'
 import Dashboard from './layout.js'
 import 'ant-design-pro/dist/ant-design-pro.css';
 import getWeb3 from './../utils/getWeb3';
+import MarketPlace from '../../build/contracts/MarketPlace.json'
 
- App extends React.Component {
+class App extends React.Component {
 
   constructor(props){
-    super(props);
-    const Contract = window.web3.eth.contract();
-
+    super(props)
+    // const Contract = window.web3.eth.contract();
 
     this.state={
-        ContractInstance: Contract.at('');
-    }
+        web3: null,
+        account: '0x0',
 
+    }
+  }
+
+  componentWillMount(){
     // Setup Web3 Connection
-    getWeb3
-    .then(results => {
+    getWeb3.then(results => {
       this.setState({
         web3: results.web3
       })
 
       // Instantiate contract once web3 provided.
       this.instantiateContract()
-    })
-    .catch(() => {
+    }).catch(() => {
       console.log('Error finding web3.')
     })
 
-
   }
 
-  componentDidMount() {
 
+  instantiateContract(){
+    console.log("instantiateContract");
+
+    var contract = require('truffle-contract');
+    var marketPlace = contract(MarketPlace)
+    marketPlace.setProvider(this.state.web3.currentProvider)
+    var marketInstance
+
+
+    this.state.web3.eth.getAccounts((error, account) => {
+      console.log('Get Accounts');
+      this.setState({account})
+      marketPlace.deployed().then((instance) => {
+        marketInstance = instance
+      })
+    })
   }
 
 
   render(){
-    return <Dashboard/>
+    return <Dashboard account={this.state.account}/>
   }
 
 }
 
 ReactDOM.render(
-   <Dashboard/>,
+   <App />,
    document.getElementById('app')
 )
 
--- src/utils/getWeb3.js
+++ src/utils/getWeb3.js
@@ -8,17 +8,17 @@ let getWeb3 = new Promise(function(resolve, reject) {
 
     // Checking if Web3 has been injected by the browser (Mist/MetaMask)
     if (typeof web3 !== 'undefined') {
+
       // Use Mist/MetaMask's provider.
       web3 = new Web3(web3.currentProvider)
 
       results = {
         web3: web3
       }
-
       console.log('Injected web3 detected.');
-
       resolve(results)
     } else {
+
       // Fallback to localhost if no web3 injection. We've configured this to
       // use the development console's port by default.
       var provider = new Web3.providers.HttpProvider('http://127.0.0.1:9545')
