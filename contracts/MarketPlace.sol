b1c350be59709bdd0e800b45b44635d0be1030ab
Kevin Ma <1043258912@qq.com>
2018-05-23 00:44:34 +1000

    change

--- contracts/MarketPlace.sol
+++ contracts/MarketPlace.sol
@@ -4,46 +4,68 @@ import './Model.sol';
 
 contract MarketPlace {
 
-  string[] categories;
-  address owner;
-  Model model;
+    string[] categories;
+    address owner;
 
-  function MarketPlace() public{
-    owner = msg.sender;
-  }
+    uint public model_count;
+    mapping (address => User) users;
+    mapping (address => address[]) model_by_user;
+    mapping (uint => Model) models;
 
-  function create_model_contract()
-    public
-    returns
-    (address model_contract)
-  {
-    model = Model(msg.sender);
-    return model;
-  }
+    mapping (address => bytes[]) ipfs_by_address;
+    mapping (address => uint[]) contestId;
 
-  function() public payable {}
 
-  struct User {
-    address user_address;
-    bytes[] submitted_data;
-    uint balance;
-    uint[] models;
-    uint[] contests;
-  }
+    uint public best_submission_index;
+    int256 public best_submission_accuracy = 0;
 
-  mapping (address => User) users;
+    constructor() public{
+      owner = msg.sender;
+    }
 
-  function kill() public{
-    if(msg.sender==owner)
-      selfdestruct(owner);
-  }
+    function() public payable {}
 
-  /* function upload() public{
+    struct User {
+      address user_address;
+      uint balance;
+    }
 
-  } */
+    function create_model(string _description, bytes _ipfs, uint _parent)
+    public returns (bool)
+    {
+      uint id = model_count + 1;
+      Model new_model =new Model(msg.sender, id, _description, _ipfs, _parent);
+      model_by_user[msg.sender].push(new_model);
+      models[id] = new_model;
+      model_count = model_count + 1;
+      return true;
+    }
 
-  function get_model(uint id) public view
-    returns(uint, bytes32, int256, bytes32, int256){
-    return model.get_Model(id);
-  }
+    function set_accuracy(uint id, int256 _accuarcy) public {
+        require(model_by_user[msg.sender].length != 0);
+        models[id].set_accuracy(msg.sender, _accuarcy);
+    }
+
+
+    function get_model_desc(uint _id) public view
+    returns(string){
+        return models[_id].get_name();
+    }
+
+    function get_model_by_id(uint _id) public view returns(address){
+        return models[_id];
+    }
+
+    function get_all_model_by_user(address _user) public view returns (address[]){
+      address[] storage model_list = model_by_user[_user];
+      return model_list;
+    }
+
+    function set_ipfshash( bytes _ipfs, uint _id) public{
+        models[_id].set_ipfs(msg.sender, _ipfs);
+    }
+
+    function get_model_count() public view returns(uint){
+        return model_count;
+    }
 }
--- contracts/Model.sol
+++ contracts/Model.sol
@@ -1,92 +1,68 @@
 pragma solidity ^0.4.18;
 
 contract Model {
-  address owner;
-  function Model() public {
-    owner = msg.sender;
+  address public owner;
+  bytes public ipfs_address;
+  uint public id;
+  string public description;
+  uint public parent;
+  uint[] children;
+  bool genesis;
+  int256 public accuracy;
+  bytes32 category;
+  uint level;
+  uint count;
+  int256 public price;
+  address factory;
+
+
+  // Constructor
+  constructor(
+	  address _owner, uint _id, string _description, bytes _ipfs, uint _parent)
+	  public {
+	owner = _owner;
+	factory = msg.sender;
+	id = _id;
+	description = _description;
+	ipfs_address = _ipfs;
+	parent = _parent;
   }
 
-
-  struct Node {
-    bytes ipfs_address;
-    uint id;
-    bytes32 description;
-    uint parent;
-    uint[] children;
-    bool genesis;
-    int256 accuracy;
-    bytes32 category;
-    uint level;
-    uint count;
-    int256 price;
+  modifier isOwner(address _caller) {
+	  require(msg.sender == factory);
+	  require(_caller == owner);
+	  _;
   }
 
-  address public organizer;
-  uint public best_submission_index;
-  int256 public best_submission_accuracy = 0;
+	function set_accuracy(address caller, int256 _accuracy) public isOwner(caller) {
+	   accuracy  = _accuracy;
+	}
+
   bytes32[] public categories;
 
-  mapping (uint => Node) nodes;
-  uint[] public models;
-  mapping (bytes32 => uint[]) categoryModels;
-
-
-  function get_model_from_category(bytes32 category)
-    view public
-    returns (uint[])
-  {
-    return categoryModels[category];
+  function append_child(uint _child) public{
+	  children.push(_child);
   }
 
-
-  function add_category(bytes32 new_category)
-    public
-    returns (bool)
-  {
-    require(check_category(new_category)==false);
-    uint[] memory temp;
-    categoryModels[new_category] = temp;
-    categories.push(new_category);
-    return true;
+  function get_children() public view returns (uint[]){
+	return children;
   }
 
-
-  function check_category(bytes32 category)
-    public view
-    returns (bool exist)
-  {
-    for (uint i = 0; i < categories.length; i++){
-      if(keccak256(categories[i]) == keccak256(category)){
-        return false;
-      }
-    }
-    return true;
+  function get_parent() public view returns (uint){
+	return parent;
   }
 
-  // For now Solidity can't return variably-sized data
-  /* function get_categories() public view returns (string[] categories){
+	function set_ipfs(address caller, bytes _ipfs) public isOwner(caller){
+		ipfs_address = _ipfs;
+	}
 
-  }  */
-
-  function get_model_count() public view returns(uint count){
-    return models.length;
+  function get_name() public view returns(string){
+	  return description;
   }
 
-  function insert_node(uint parent, uint node) public {
-    nodes[node].parent = parent;
-    nodes[parent].children.push(node);
+  function get_Model() public constant returns(uint, string, int256, bytes32, int256){
+
+	return (id, description, accuracy, category, price);
   }
 
-  function get_children(uint node) public view returns (uint[] child){
-    return nodes[node].children;
-  }
-
-  function get_parent(uint node) public view returns (uint parent){
-    return nodes[node].parent;
-  }
-
-  function get_Model(uint node) public constant returns(uint, bytes32, int256, bytes32, int256){
-    Node memory node_ = nodes[node];
-    return (node_.id, node_.description, node_.accuracy, node_.category, node_.price);
-  }
 }
